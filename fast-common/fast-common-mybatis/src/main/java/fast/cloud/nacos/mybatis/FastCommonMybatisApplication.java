ae22ea8a04e269d0fcffa3d629f02dbbee5e5c5d
qfx <qinfuxiang>
2019-10-21 18:14:28 +0800

    mybatis init

---
+++ fast-common/fast-common-mybatis/src/main/java/fast/cloud/nacos/mybatis/FastCommonMybatisApplication.java
@@ -0,0 +1,13 @@
+package fast.cloud.nacos.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCommonMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastCommonMybatisApplication.class, args);
+    }
+
+}
