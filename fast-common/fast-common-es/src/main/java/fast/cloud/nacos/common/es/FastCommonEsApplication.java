16bb250ff447cf7ca1a23e10c8300bafa6a5701a
qfx <qinfuxiang>
2019-10-23 18:22:24 +0800

    add es

---
+++ fast-common/fast-common-es/src/main/java/fast/cloud/nacos/common/es/FastCommonEsApplication.java
@@ -0,0 +1,13 @@
+package fast.cloud.nacos.common.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCommonEsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastCommonEsApplication.class, args);
+    }
+
+}
---
+++ fast-common/fast-common-es/src/test/java/fast/cloud/nacos/common/es/FastCommonEsApplicationTests.java
@@ -0,0 +1,13 @@
+package fast.cloud.nacos.common.es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastCommonEsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
