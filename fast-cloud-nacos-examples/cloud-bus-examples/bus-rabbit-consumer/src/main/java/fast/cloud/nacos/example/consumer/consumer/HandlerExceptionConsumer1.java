7f2a36fd1fe9c84ba637273aae86c011b2763bca
qfx <qinfuxiang>
2019-10-28 12:01:05 +0800

    Spring Cloud Stream 消息降级

--- fast-cloud-nacos-examples/cloud-bus-examples/bus-rabbit-consumer/src/main/java/fast/cloud/nacos/example/consumer/consumer/HandlerExceptionConsumer1.java
+++ fast-cloud-nacos-examples/cloud-bus-examples/bus-rabbit-consumer/src/main/java/fast/cloud/nacos/example/consumer/consumer/HandlerExceptionConsumer1.java
@@ -4,6 +4,8 @@ import fast.cloud.nacos.example.consumer.channel.HandlerExceptionChannel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -14,5 +16,15 @@ public class HandlerExceptionConsumer1 {
     public void consume(String message) {
         log.info("接收消息：{}", message);
         throw new RuntimeException("消费异常");
+    }
+
+    /**
+     * 消息消费失败的降级处理逻辑
+     * destination + group
+     * @param message
+     */
+    @ServiceActivator(inputChannel = "handler-exception1-topic.handler-exception1.errors")
+    public void error(Message<?> message) {
+        log.info("Message consumer failed, call fallback!");
     }
 }
