7dfd3569ba0686e7826735f7aa3730f3ce45a8d7
qfx <qinfuxiang>
2019-10-31 16:05:21 +0800

    provider add sentinel

--- fast-cloud-nacos-examples/sentinel-examples/service-provider-sentinel/src/main/java/fast/cloud/nacos/sentinel/provider/ServiceProviderSentinelApplication.java
+++ fast-cloud-nacos-examples/sentinel-examples/service-provider-sentinel/src/main/java/fast/cloud/nacos/sentinel/provider/ServiceProviderSentinelApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServiceProviderSentinelApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ServiceProviderSentinelApplication.class, args);
     }
-
 }
