373ef67750ed20ef2e2a2871028c6bcbece2cad1
qfx <qinfuxiang>
2019-11-08 17:40:27 +0800

    添加断路器以及配置

--- fast-cloud-nacos-examples/cloud-rpc-examples/service-feign/src/main/java/fast/cloud/nacos/ServiceFeignApplication.java
+++ fast-cloud-nacos-examples/cloud-rpc-examples/service-feign/src/main/java/fast/cloud/nacos/ServiceFeignApplication.java
@@ -2,12 +2,14 @@ package fast.cloud.nacos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
+@EnableCircuitBreaker
 //@ComponentScan(basePackages = {"com.batman", "fast.cloud.nacos"})
 public class ServiceFeignApplication {
     public static void main(String[] args) {
