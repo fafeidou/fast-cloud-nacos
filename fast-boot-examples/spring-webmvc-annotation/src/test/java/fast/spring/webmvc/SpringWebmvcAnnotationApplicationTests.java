13331e5487e922b7caccf8d153686724a2a745f3
qfx <qinfuxiang>
2019-11-13 20:14:34 +0800

    add spring web mvc annotation

---
+++ fast-boot-examples/spring-webmvc-annotation/src/test/java/fast/spring/webmvc/SpringWebmvcAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package fast.spring.webmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebmvcAnnotationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
