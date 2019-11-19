2b053ac4685918385dbd8281b6180e0dcd5e15ac
qfx <qinfuxiang>
2019-11-19 09:36:58 +0800

    fixed

--- fast-common/fast-common-model/src/main/java/fast/cloud/nacos/common/model/condition/BaseCommonPaginationSearchRequest.java
+++ fast-common/fast-common-model/src/main/java/fast/cloud/nacos/common/model/condition/BaseCommonPaginationSearchRequest.java
@@ -2,7 +2,9 @@ package fast.cloud.nacos.common.model.condition;
 
 import fast.cloud.nacos.common.model.request.CommonSearchRequest;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class BaseCommonPaginationSearchRequest<T extends SearchCondition> extends CommonSearchRequest<T> {
     private PageCondition pagination;
--- fast-common/fast-common-model/src/main/java/fast/cloud/nacos/common/model/response/PaginationResponse.java
+++ fast-common/fast-common-model/src/main/java/fast/cloud/nacos/common/model/response/PaginationResponse.java
@@ -1,7 +1,9 @@
 package fast.cloud.nacos.common.model.response;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class PaginationResponse<T> extends BaseResponse<T> {
     private Long total;
