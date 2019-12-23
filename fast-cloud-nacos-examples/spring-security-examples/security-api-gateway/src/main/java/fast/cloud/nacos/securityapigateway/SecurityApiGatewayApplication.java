25fca4a7f339f915a131a626f8670242c096d294
qfx <qinfuxiang>
2019-12-23 16:29:37 +0800

    add security api gateway

---
+++ fast-cloud-nacos-examples/spring-security-examples/security-api-gateway/src/main/java/fast/cloud/nacos/securityapigateway/SecurityApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package fast.cloud.nacos.securityapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApiGatewayApplication.class, args);
+    }
+
+}
