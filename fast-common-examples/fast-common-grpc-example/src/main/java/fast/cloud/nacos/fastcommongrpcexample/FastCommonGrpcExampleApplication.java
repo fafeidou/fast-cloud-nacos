52f63e33403d0a75d299f8ffed031a4a4dd31cf4
qfx <qinfuxiang>
2020-01-03 09:57:18 +0800

    init grpc

---
+++ fast-common-examples/fast-common-grpc-example/src/main/java/fast/cloud/nacos/fastcommongrpcexample/FastCommonGrpcExampleApplication.java
@@ -0,0 +1,13 @@
+package fast.cloud.nacos.fastcommongrpcexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCommonGrpcExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastCommonGrpcExampleApplication.class, args);
+    }
+
+}
