c66d36606cf03436b88b872a82baa9dad0252332
qfx <qinfuxiang>
2020-01-15 14:30:18 +0800

    add source code module

---
+++ fast-source-code-analysis/code-mybatis/src/main/java/fast/cloud/nacos/mybatis/CodeMybatisApplication.java
@@ -0,0 +1,13 @@
+package fast.cloud.nacos.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeMybatisApplication.class, args);
+    }
+
+}
---
+++ fast-source-code-analysis/code-mybatis/src/test/java/fast/cloud/nacos/mybatis/CodeMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package fast.cloud.nacos.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
