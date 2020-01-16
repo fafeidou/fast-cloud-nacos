6e6b3a1a12b131876bf27607a35634a23977a0f5
qfx <qinfuxiang>
2020-01-16 10:34:21 +0800

    init common custom mybatis

---
+++ fast-common/fast-common-custom-mybatis/src/main/java/fast/cloud/nacos/custom/mybatis/FastCommonCustomMybatisApplication.java
@@ -0,0 +1,13 @@
+package fast.cloud.nacos.custom.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCommonCustomMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastCommonCustomMybatisApplication.class, args);
+	}
+
+}
---
+++ fast-common/fast-common-custom-mybatis/src/test/java/fast/cloud/nacos/custom/mybatis/FastCommonCustomMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package fast.cloud.nacos.custom.mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastCommonCustomMybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
