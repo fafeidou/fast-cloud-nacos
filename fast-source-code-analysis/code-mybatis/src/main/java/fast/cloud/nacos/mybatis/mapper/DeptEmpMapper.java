9bed931284db85f72575dc33123442e455cc5c20
qfx <qinfuxiang>
2020-01-17 10:03:07 +0800

    自定义注解查询

--- fast-source-code-analysis/code-mybatis/src/main/java/fast/cloud/nacos/mybatis/mapper/DeptEmpMapper.java
+++ fast-source-code-analysis/code-mybatis/src/main/java/fast/cloud/nacos/mybatis/mapper/DeptEmpMapper.java
@@ -1,5 +1,6 @@
 package fast.cloud.nacos.mybatis.mapper;
 
+import fast.cloud.nacos.custom.mybatis.annotation.Select;
 import fast.cloud.nacos.mybatis.entity.DeptEmp;
 
 import java.util.List;
@@ -7,6 +8,7 @@ import java.util.List;
 //Mapper接口  不用编写实现类
 public interface DeptEmpMapper {
 
+    @Select("SELECT deptno, COUNT(*) AS total  FROM  emp  GROUP BY deptno")
     List<DeptEmp> getEmpTotalByDept();
 
 }
--- fast-source-code-analysis/code-mybatis/src/test/java/fast/cloud/nacos/mybatis/CustomMybatisTests.java
+++ fast-source-code-analysis/code-mybatis/src/test/java/fast/cloud/nacos/mybatis/CustomMybatisTests.java
@@ -40,4 +40,6 @@ public class CustomMybatisTests {
 
     }
 
+
+
 }
