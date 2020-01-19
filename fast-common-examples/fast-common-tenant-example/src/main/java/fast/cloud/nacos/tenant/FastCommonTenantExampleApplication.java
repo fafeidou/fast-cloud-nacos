70f10765b84bb873851ab78fed8c5392a58c7a56
qfx <qinfuxiang>
2020-01-19 21:12:09 +0800

    init tenant

---
+++ fast-common-examples/fast-common-tenant-example/src/main/java/fast/cloud/nacos/tenant/FastCommonTenantExampleApplication.java
@@ -0,0 +1,13 @@
+package fast.cloud.nacos.tenant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCommonTenantExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastCommonTenantExampleApplication.class, args);
+	}
+
+}
---
+++ fast-common-examples/fast-common-tenant-example/src/test/java/fast/cloud/nacos/tenant/FastCommonTenantExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package fast.cloud.nacos.tenant;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class FastCommonTenantExampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
