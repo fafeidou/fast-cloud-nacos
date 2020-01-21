5d89f8f169a9c2a69636a188b9577c6b2574c039
qfx <qinfuxiang>
2020-01-21 11:19:56 +0800

    add rocket mq

--- fast-common-examples/fast-common-rocketmq-example/src/main/java/fast/cloud/nacos/rocketmq/example/FastCommonRocketmqExampleApplication.java
+++ fast-common-examples/fast-common-rocketmq-example/src/main/java/fast/cloud/nacos/rocketmq/example/FastCommonRocketmqExampleApplication.java
@@ -1,13 +1,40 @@
 package fast.cloud.nacos.rocketmq.example;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 @SpringBootApplication
-public class FastCommonRocketmqExampleApplication {
+public class FastCommonRocketmqExampleApplication implements CommandLineRunner {
+    @Resource
+    private RocketMQTemplate rocketMQTemplate;
 
     public static void main(String[] args) {
         SpringApplication.run(FastCommonRocketmqExampleApplication.class, args);
     }
+
+    @Override
+    public void run(String... args) throws Exception {
+        rocketMQTemplate.convertAndSend("test-topic-1", "Hello, World!");
+        rocketMQTemplate.send("test-topic-1", MessageBuilder.withPayload("Hello, World! I'm from spring message").build());
+    }
+
+
+//    @Slf4j
+//    @Service
+//    @RocketMQMessageListener(topic = "test-topic-1", consumerGroup = "my-consumer_test-topic-1")
+//    public static class MyConsumer1 implements RocketMQListener<String> {
+//        public void onMessage(String message) {
+//            log.info("received message: {}", message);
+//        }
+//    }
 
 }
