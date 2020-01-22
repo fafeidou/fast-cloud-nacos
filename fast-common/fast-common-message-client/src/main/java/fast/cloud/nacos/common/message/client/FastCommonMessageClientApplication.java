cd8169577be7ef1210e90f8908afa05e30961a74
qfx <qinfuxiang>
2020-01-22 23:56:46 +0800

    message-client init

---
+++ fast-common/fast-common-message-client/src/main/java/fast/cloud/nacos/common/message/client/FastCommonMessageClientApplication.java
@@ -0,0 +1,13 @@
+package fast.cloud.nacos.common.message.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCommonMessageClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastCommonMessageClientApplication.class, args);
+    }
+
+}
---
+++ fast-common/fast-common-message-client/src/test/java/fast/cloud/nacos/common/message/client/FastCommonMessageClientApplicationTests.java
@@ -0,0 +1,16 @@
+package fast.cloud.nacos.common.message.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FastCommonMessageClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
