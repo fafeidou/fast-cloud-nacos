a99e25b6c2607a0c223c14dfb9780152f8609b7a
qfx <qinfuxiang>
2020-01-26 15:41:56 +0800

    修改打成war包

--- fast-common-examples/fast-common-tenant-example/src/main/java/fast/cloud/nacos/tenant/FastCommonTenantExampleApplication.java
+++ fast-common-examples/fast-common-tenant-example/src/main/java/fast/cloud/nacos/tenant/FastCommonTenantExampleApplication.java
@@ -3,10 +3,12 @@ package fast.cloud.nacos.tenant;
 import fast.cloud.nacos.tenant.dynamic.mybatis.DynamicDataSourceAspect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication(scanBasePackages = {"fast.cloud.nacos.tenant", "fast.cloud.nacos.common.tenant"})
-public class FastCommonTenantExampleApplication {
+public class FastCommonTenantExampleApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(FastCommonTenantExampleApplication.class, args);
@@ -16,4 +18,8 @@ public class FastCommonTenantExampleApplication {
         System.out.println(bean);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(FastCommonTenantExampleApplication.class);
+    }
 }
