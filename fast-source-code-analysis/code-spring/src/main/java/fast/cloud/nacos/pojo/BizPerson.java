63d05b9152573d622add3a69048981c7dd45591a
qfx <qinfuxiang>
2020-01-28 12:45:40 +0800

    测试spring 生命周期

--- fast-source-code-analysis/code-spring/src/main/java/fast/cloud/nacos/pojo/BizPerson.java
+++ fast-source-code-analysis/code-spring/src/main/java/fast/cloud/nacos/pojo/BizPerson.java
@@ -1,13 +1,18 @@
 package fast.cloud.nacos.pojo;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
-public class BizPerson implements Person {
-
-
+public class BizPerson implements Person, BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
     private Animal animal;
 
     @Override
@@ -21,5 +26,40 @@ public class BizPerson implements Person {
     public void setAnimal(Animal animal) {
         System.out.println("延迟依赖注入");
         this.animal = animal;
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println(this.getClass().getSimpleName() + "调用BeanFactoryAware的setBeanFactory方法");
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println(this.getClass().getSimpleName() + "调用BeanNameAware的setBeanName方法");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println(this.getClass().getSimpleName() + "调用DisposableBean的destroy方法");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println(this.getClass().getSimpleName() + "调用InitializingBean的afterPropertiesSet方法");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println(this.getClass().getSimpleName() + "调用ApplicationContextAware的setApplicationContext方法");
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println(this.getClass().getSimpleName() + "调用@PostConstruct自定义的初始化方法");
+    }
+
+    @PreDestroy
+    public void destroy1() {
+        System.out.println(this.getClass().getSimpleName() + "调用@PreDestroy自定义的销毁方法");
     }
 }
