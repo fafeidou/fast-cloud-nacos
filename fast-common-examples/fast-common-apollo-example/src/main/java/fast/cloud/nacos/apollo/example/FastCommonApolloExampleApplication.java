0442ab55773fa305f1508e686482d6d0906319b4
qinfuxiang <17521222912@163.com>
2020-03-27 10:23:58 +0800

    springboot配置apollo

--- fast-common-examples/fast-common-apollo-example/src/main/java/fast/cloud/nacos/apollo/example/FastCommonApolloExampleApplication.java
+++ fast-common-examples/fast-common-apollo-example/src/main/java/fast/cloud/nacos/apollo/example/FastCommonApolloExampleApplication.java
@@ -1,13 +1,31 @@
 package fast.cloud.nacos.apollo.example;
 
+import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.ConfigService;
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
-public class FastCommonApolloExampleApplication {
+@EnableApolloConfig
+public class FastCommonApolloExampleApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(FastCommonApolloExampleApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Config config = ConfigService.getAppConfig();
+        String someKey = "sms.enable";
+        while (true) {
+            String value = config.getProperty(someKey, null);
+            System.out.printf("now: %s, sms.enable: %s%n", LocalDateTime.now().toString(),
+                    value);
+            Thread.sleep(3000L);
+        }
+    }
 }
