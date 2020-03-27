d6ddf1915aec4a190a386ebc4338d17e48be7a29
qinfuxiang <17521222912@163.com>
2020-03-27 14:03:39 +0800

    sharding jdbc 数据脱敏

---
+++ fast-common-examples/fast-common-sharding-encryptor-example/src/main/java/fast/cloud/nacos/sharding/encryptor/FastCommonShardingEncryptorExampleApplication.java
@@ -0,0 +1,13 @@
+package fast.cloud.nacos.sharding.encryptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCommonShardingEncryptorExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastCommonShardingEncryptorExampleApplication.class, args);
+    }
+
+}
---
+++ fast-common-examples/fast-common-sharding-encryptor-example/src/test/java/fast/cloud/nacos/sharding/encryptor/FastCommonShardingEncryptorExampleApplicationTests.java
@@ -0,0 +1,35 @@
+package fast.cloud.nacos.sharding.encryptor;
+
+import fast.cloud.nacos.sharding.encryptor.dao.UserDao;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@SpringBootTest(classes = FastCommonShardingEncryptorExampleApplication.class)
+public class FastCommonShardingEncryptorExampleApplicationTests {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Test
+    public void testInsertUser() {
+        for (int i = 20; i < 30; i++) {
+            Long id = i + 1L;
+            userDao.insertUser(id, "姓名" + id);
+        }
+    }
+
+    @Test
+    public void testSelectUserbyIds() {
+        List<Long> userIds = new ArrayList<>();
+        userIds.add(21L);
+        userIds.add(22L);
+        List<Map> users = userDao.selectUserbyIds(userIds);
+        System.out.println(users);
+    }
+
+}
