180962d983f0b8149ad9bd54e335fdec899a8531
qinfuxiang <17521222912@163.com>
2020-07-07 17:50:06 +0800

    feat(请求参数上报skywalking): 请求参数上报skywalking

--- fast-common/fast-common-model/src/main/java/fast/cloud/nacos/common/model/log/RequestFilter.java
+++ fast-common/fast-common-model/src/main/java/fast/cloud/nacos/common/model/log/RequestFilter.java
@@ -1,18 +1,27 @@
 package fast.cloud.nacos.common.model.log;
 
 import fast.cloud.nacos.common.model.beans.context.ProjectContext;
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.MDC;
-
-import javax.servlet.*;
-import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpServletRequest;
+import fast.cloud.nacos.common.model.utils.GsonUtil;
 import java.io.IOException;
 import java.util.Objects;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.skywalking.apm.toolkit.trace.ActiveSpan;
+import org.apache.skywalking.apm.toolkit.trace.Trace;
+import org.slf4j.MDC;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Slf4j
 @WebFilter(filterName = "requestWrapperFilter", urlPatterns = "/*")
 public class RequestFilter implements Filter {
+
     public static final String TRACE_ID = "trace";
     public static final String SPAN_ID = "span";
 
@@ -24,7 +33,9 @@ public class RequestFilter implements Filter {
      * 初始化请求链路信息：唯一key，日志初始化，body包装防止获取日志打印时后续不能继续使用
      */
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    @Trace(operationName = "requestWrapperFilter")
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+        throws IOException, ServletException {
         String contextString = ((HttpServletRequest) request).getHeader(ProjectContext.CONTEXT_KEY);
         if (Objects.nonNull(contextString)) {
             ProjectContext.fromString(contextString);
@@ -33,6 +44,23 @@ public class RequestFilter implements Filter {
             ProjectContext.initContext();
         }
         initLog();
+
+        CustomHttpServletRequestWrapper requestWrapper = new CustomHttpServletRequestWrapper(
+            (HttpServletRequest) request);
+        String uri = requestWrapper.getRequestURI();
+        String remoteAddr = requestWrapper.getRemoteAddr();
+        String method = requestWrapper.getMethod();
+
+        String requestStr;
+
+        if (method.equals(RequestMethod.GET.name())) {
+            requestStr = GsonUtil.toJson(requestWrapper.getParameterMap());
+        } else {
+            requestStr = requestWrapper.getBody();
+        }
+        ActiveSpan.tag("request", requestStr);
+        log.info(
+            "url:{} , method:{}, clientIp:{}, params:{}", uri, method, remoteAddr, requestStr);
         chain.doFilter(request, response);
     }
 
