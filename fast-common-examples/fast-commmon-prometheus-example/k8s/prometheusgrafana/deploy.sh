dba4e00a433852181922652a201a9d5b5b906f75
qinfuxiang <17521222912@163.com>
2021-01-31 17:25:53 +0800

    k8s prometheus shell

--- fast-common-examples/fast-commmon-prometheus-example/k8s/prometheusgrafana/deploy.sh
+++ fast-common-examples/fast-commmon-prometheus-example/k8s/prometheusgrafana/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-kubectl create -f ../grafana/node-exporter.yaml
-kubectl create -f ../prometheus/rbac-setup.yaml
-kubectl create -f ../prometheus/configmap.yaml
-kubectl create -f ../prometheus/prometheus.yaml
-kubectl create -f ../prometheus/grafana.yaml
+kubectl create -f ./grafana/node-exporter.yaml
+kubectl create -f ./prometheus/rbac-setup.yaml
+kubectl create -f ./prometheus/configmap.yaml
+kubectl create -f ./prometheus/prometheus.yaml
+kubectl create -f ./prometheus/grafana.yaml
 
 echo "create prometheus and grafana successful"
