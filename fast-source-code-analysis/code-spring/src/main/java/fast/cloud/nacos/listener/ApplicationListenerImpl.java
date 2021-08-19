72f84e15115cc1efa77c0142e03e1fbea5e63a2a
qinfuxiang <batman.qin@huolala.cn>
2021-08-19 11:45:39 +0800

    spring event 学习

--- fast-source-code-analysis/code-spring/src/main/java/fast/cloud/nacos/listener/ApplicationListenerImpl.java
+++ fast-source-code-analysis/code-spring/src/main/java/fast/cloud/nacos/listener/ApplicationListenerImpl.java
@@ -3,9 +3,8 @@ package fast.cloud.nacos.listener;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
-import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 @Slf4j
 public class ApplicationListenerImpl implements ApplicationListener<ApplicationEvent> {
 
