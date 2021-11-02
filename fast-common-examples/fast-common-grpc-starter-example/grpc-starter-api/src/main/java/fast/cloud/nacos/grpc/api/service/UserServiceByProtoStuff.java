603b53e02a72174c9c1f52c94afb9889ffa237fa
qinfuxiang <batman.qin@huolala.cn>
2021-11-02 18:07:19 +0800

    自定义注解注入

--- fast-common-examples/fast-common-grpc-starter-example/grpc-starter-api/src/main/java/fast/cloud/nacos/grpc/api/service/UserServiceByProtoStuff.java
+++ fast-common-examples/fast-common-grpc-starter-example/grpc-starter-api/src/main/java/fast/cloud/nacos/grpc/api/service/UserServiceByProtoStuff.java
@@ -6,7 +6,7 @@ import fast.cloud.nacos.grpc.starter.annotation.GrpcService;
 import fast.cloud.nacos.grpc.starter.constant.SerializeType;
 import java.util.List;
 
-@GrpcService(server = "user", serialization = SerializeType.PROTOSTUFF,grpcServer = "user-grpc")
+@GrpcService(server = "user", serialization = SerializeType.PROTOSTUFF,grpcServer = "user-grpc",host = "127.0.0.1",port = 6566)
 public interface UserServiceByProtoStuff {
 
     void insert(UserEntity userEntity);
--- fast-common-examples/fast-common-grpc-starter-example/grpc-starter-client/src/main/java/fast/cloud/nacos/grpc/starter/client/controller/V2UserController.java
+++ fast-common-examples/fast-common-grpc-starter-example/grpc-starter-client/src/main/java/fast/cloud/nacos/grpc/starter/client/controller/V2UserController.java
@@ -2,21 +2,19 @@ package fast.cloud.nacos.grpc.starter.client.controller;
 
 import fast.cloud.nacos.grpc.api.entity.UserEntity;
 import fast.cloud.nacos.grpc.api.service.UserServiceByProtoStuff;
-import java.util.List;
+import fast.cloud.nacos.grpc.starter.annotation.GrpcReference;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/v2/user")
 public class V2UserController {
 
-    private final UserServiceByProtoStuff userServiceByProtoStuff;
+    @GrpcReference
+    private UserServiceByProtoStuff userServiceByProtoStuff;
 
     @PostMapping("/add")
     public UserEntity insertUser(@RequestBody UserEntity userEntity){
