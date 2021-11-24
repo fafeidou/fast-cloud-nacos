9d94c37cf7d8130b4b9db7d5ec67703a3e42f0b0
qinfuxiang <batman.qin@huolala.cn>
2021-11-24 17:24:59 +0800

    Autowired 理解

--- fast-boot-examples/auto-configure/src/main/java/fast/boot/autoconfigure/autowired/AutowiredBootstrap.java
+++ fast-boot-examples/auto-configure/src/main/java/fast/boot/autoconfigure/autowired/AutowiredBootstrap.java
@@ -1,14 +1,35 @@
 package fast.boot.autoconfigure.autowired;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@SpringBootApplication
+@ComponentScan
+@Configuration
 public class AutowiredBootstrap {
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(AutowiredBootstrap.class, args);
-        A a = context.getBean(A.class);
-        System.out.println(a);
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AutowiredBootstrap.class);
+        A a = applicationContext.getBean(A.class);
+        a.test();
+        applicationContext.registerShutdownHook();
     }
+
+    @Component
+    public class A {
+
+        @Autowired
+        private B b;
+
+        public void test() {
+            System.out.println(b);
+        }
+
+    }
+
+    @Component
+    public class B {
+    }
+
 }
