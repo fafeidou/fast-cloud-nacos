2948eba5076e6dfee5edffcc1eeb4defebf8eb3e
qinfuxiang <batman.qin@huolala.cn>
2022-02-07 17:32:01 +0800

    dubbo study

--- fast-cloud-nacos-examples/dubbo-examples/fast-dubbo-server/src/main/java/fast/cloud/nacos/dubbo/server/FastDubboServerApplication.java
+++ fast-cloud-nacos-examples/dubbo-examples/fast-dubbo-server/src/main/java/fast/cloud/nacos/dubbo/server/FastDubboServerApplication.java
@@ -1,5 +1,7 @@
 package fast.cloud.nacos.dubbo.server;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import fast.cloud.nacos.dubbo.api.HelloService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -7,6 +9,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class FastDubboServerApplication {
+    @Reference
+    HelloService helloService;
 
     public static void main(String[] args) {
         SpringApplication.run(FastDubboServerApplication.class, args);
